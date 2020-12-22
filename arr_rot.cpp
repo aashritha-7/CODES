8bcb211c8ede70a2afd314e0428ed2f1a7293f7e
aashritha <71088977+aashritha-7@users.noreply.github.com>
2020-12-22 20:19:20 +0530

    Update arr_rot.cpp

--- arr_rot.cpp
+++ arr_rot.cpp
@@ -1,36 +1,24 @@
-#include<iostream>                    //counterclock-wise
+#include <bits/stdc++.h>
 using namespace std;
-void rev(int a[],int l ,int h)
-{
-    while(l<h)
-    {
-        int t;
-        t = a[l];
-        a[l] = a[h];
-        a[h] = t;
-        l++;
-        h--;
-    }
-}
-int print(int a[] , int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        cout<<a[i]<<" ";
-    }
-}
 int main()
 {
-    int n , d;
+    int a[10];
+    int n,i,k;
     cin>>n;
-    cin>>d;
-    int arr[n];
     for(int i=0;i<n;i++)
     {
-        cin>>arr[i];
+        cin>>a[i];
     }
-    rev(arr , 0 , d-1);
-    rev(arr , d , n-1);
-    rev(arr , 0 , n-1);
-    print(arr , n);
+    cin>>k;
+    //lEFT ROTATION
+    for(i=0;i<n;i++)
+    {
+        cout<<a[(i+k+1)%n];
+    }
+    // RIGHT ROTATION
+    for(i=0;i<n;i++)
+    {
+        cout<<a[(i+k+1)%n];
+    }
+    return 0;
 }
